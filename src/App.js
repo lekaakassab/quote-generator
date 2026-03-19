e230341d71b13982b68d164818427d87c21ded0e
lekaa kassab <lekaakassab7@gmail.com>
2026-03-19 02:57:35 +0100

    quote generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+
+  const getQuote = async () => {
+    try {
+      const response = await fetch("https://motivational-spark-api.vercel.app/api/quotes/random");
+      const data = await response.json();
+      setQuote(data.quote);
+    } catch (error) {
+      setQuote("Failed");
+    }
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Quote Generator</h1>
+      <p>{quote}</p>
+      <button onClick={getQuote}>New Quote</button>
     </div>
   );
 }
